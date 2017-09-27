4405cc0761715fd1b2913e593a121423e826cef4
Tom Klaver <tomklav@gmail.com>
2017-09-27 12:04:22 +0200

    prepare for PyPI test

--- pyzenodo/zenodo.py
+++ pyzenodo/zenodo.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.0-alpha'
+__version__ = '0.0.2'
 
 import requests
 import re
--- setup.py
+++ setup.py
@@ -5,20 +5,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyzenodo',
-    version='1.0.0-alpha',
+    version='0.0.2',
     description='Python wrapper for the Zenodo REST API',
     author='Tom Klaver',
     author_email='t.klaver@esciencecenter.nl',
     license='Apache 2.0',
     url='https://github.com/Tommos0/pyzenodo',
+    download_url='https://github.com/Tommos0/pyzenodo/archive/0.0.2.tar.gz',
     include_package_data=True,
     keywords=['zenodo', 'pyzenodo'],
     classifiers=[
         'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
@@ -28,12 +30,11 @@ setup(
     ],
     packages=find_packages(),
     install_requires=['requests', 'Beautifulsoup4'],
-    # install_requires=['feedparser >= 5.1.0', 'pytz', 'requests', 'pathlib'],
-    long_description="""\
+    long_description="""
 A Python wrapper for the Zenodo REST API
 ---------------------------------------------
 
 Provides methods for accessing Zenodo REST API.
 
-This version requires Python 2.7.x / 3.4.x / 3.5.x / 3.6.x"""
+This version requires Python 2.7.x / 3.4.x / 3.5.x / 3.6.x / 3.7.x"""
 )
